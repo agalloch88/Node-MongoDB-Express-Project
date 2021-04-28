d8624d13c478d847eb44c4f6a30f7622ecda4158
Ryan Kirsch <agalloch88@gmail.com>
2021-04-28 07:45:25 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '150913776966532',
+        clientSecret: '12266625377fd2b3531ae5f9458b810f'
+    }
 }
